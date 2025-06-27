d1896dd7fb2d58e79da34de4ab13ee108ee08730
BlessmanJay <blessmanjay@gmail.com>
2025-06-27 07:03:47 +0100

    New Features Added: this include the 'delete button' and 'Enter key to add new items to the list item'

--- app.js
+++ app.js
@@ -14,10 +14,30 @@ function addItem() {
     const listItem = document.createElement("li");
     listItem.textContent = value;
 
+    // Create the delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "❌";
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.style.cursor = "pointer";
+    deleteButton.style.padding = 0;
+    deleteButton.style.border = "none";
+
+    // add click event to delete the item
+    deleteButton.addEventListener("click", function () {
+      listItem.remove();
+    });
+
     // Append it to the list
+    listItem.appendChild(deleteButton);
     listContainer.appendChild(listItem);
 
     // clear the input field
     inputText.value = "";
   }
+  // Enter Button Added
+  inputText.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      addItem();
+    }
+  });
 }
